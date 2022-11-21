c00defcdc1c74cd7be4ec6f5a026b3576f9b78a9
lleeoo123 <117774426+lleeoo123@users.noreply.github.com>
2022-11-21 17:16:19 -0500

    push
    push

---
+++ Basic_HTML_and_Javascript_1/Basic_Javascript_1.js
@@ -0,0 +1,7 @@
+	function My_First_Function() {
+	var String = "Kiss me I'm Irish!";
+	var result = String.fontcolor("green");
+	document.getElementByld("Irish").innerHTML =
+	result;
+ }
+alert("Hello,good people");
